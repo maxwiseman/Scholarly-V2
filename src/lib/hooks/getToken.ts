faa37b1f7d952cbb7f02ec1051d0262f0c4f7615
maxwiseman <13maxwellwise@gmail.com>
2023-06-14 14:21:07 -0400

    Optimized getToken and added a fallback

--- src/lib/hooks/getToken.ts
+++ src/lib/hooks/getToken.ts
@@ -2,7 +2,6 @@
 
 import { db } from '@/src/database/db'
 import { users } from '@/src/database/schema'
-import { auth, useUser } from '@clerk/nextjs'
 import { eq } from 'drizzle-orm'
 
 export async function getToken(userId: string | undefined | null) {
@@ -12,12 +11,12 @@ export async function getToken(userId: string | undefined | null) {
   }
   const token = await db
     .select({
-      id: users.id,
       canvas_api_token: users.canvas_api_token,
     })
     .from(users)
-    // .where(eq(users.id, userId))
+    .where(eq(users.id, userId))
     .then(res => {
+      if (res[0] == undefined) res = [{ canvas_api_token: 'No token found!' }]
       return res[0]
     })
   return token.canvas_api_token
