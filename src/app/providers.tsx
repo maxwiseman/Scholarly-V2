76e0ba87e906e25753f39b352b43c6c9103fbb9f
maxwiseman <13maxwellwise@gmail.com>
2023-06-24 17:32:13 -0400

    Added dark theme selector

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,37 +1,29 @@
-'use client'
+"use client";
 
-import { createContext, useEffect, useState } from 'react'
-import { getToken } from '../lib/hooks'
-import { useAuth } from '@clerk/nextjs'
-import { TooltipProvider } from '../components/ui/tooltip'
+import { createContext, useEffect, useState } from "react";
+import { getToken } from "../lib/hooks";
+import { useAuth } from "@clerk/nextjs";
+import { TooltipProvider } from "../components/ui/tooltip";
 
-export const TokenContext = createContext('')
+export const TokenContext = createContext("");
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useState<'light' | 'dark' | ''>('light')
-  const [token, setToken] = useState<string | null>()
-  const auth = useAuth()
+  const [token, setToken] = useState<string | null>();
+  const auth = useAuth();
 
   useEffect(() => {
-    setTheme(
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-        ? 'dark'
-        : 'light'
-    )
-  }, [])
-  useEffect(() => {
-    getToken(auth.userId).then(res => {
-      setToken(res)
-    })
-  })
+    getToken(auth.userId).then((res) => {
+      setToken(res);
+    });
+  });
 
   return (
     <TokenContext.Provider value={token as string}>
       <TooltipProvider>
-        <html lang='en' className={'h-[100%] ' + theme}>
+        <html lang="en" className={"h-[100%] "}>
           {children}
         </html>
       </TooltipProvider>
     </TokenContext.Provider>
-  )
+  );
 }
