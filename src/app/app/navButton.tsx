f5ac0d5b525a504aeff3920799d64fbe705241cf
maxwiseman <13maxwellwise@gmail.com>
2023-06-24 23:19:27 -0400

    Added OAuth to sign-in page

--- src/app/app/navButton.tsx
+++ src/app/app/navButton.tsx
@@ -1,96 +1,96 @@
-'use client'
+"use client";
 
-import { Button, LinkButton } from '@/src/components/ui/button'
+import { Button, LinkButton } from "@/src/components/ui/button";
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
-} from '@/src/components/ui/collapsible'
+} from "@/src/components/ui/collapsible";
 import {
   ContextMenu,
   ContextMenuContent,
   ContextMenuItem,
   ContextMenuTrigger,
-} from '@/src/components/ui/context-menu'
-import { cn } from '@/src/lib/utils'
-import { IconChevronDown, IconNotebook } from '@tabler/icons-react'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { ReactElement, cloneElement, useState } from 'react'
+} from "@/src/components/ui/context-menu";
+import { cn } from "@/src/lib/utils";
+import { IconChevronDown, IconNotebook } from "@tabler/icons-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactElement, cloneElement, useState } from "react";
 
 export function NavButton(props: {
-  href: string
-  icon: ReactElement
-  children?: any
+  href: string;
+  icon: ReactElement;
+  children?: any;
 }) {
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   return (
     <LinkButton
       href={props.href}
-      variant={pathname.startsWith(props.href) ? 'default' : 'ghost'}
-      className='w-full justify-between mb-1'
+      variant={pathname.startsWith(props.href) ? "default" : "ghost"}
+      className="w-full justify-between mb-1"
     >
-      <div className='flex flex-row gap-2 items-center'>
-        {cloneElement(props.icon, { className: 'w-4 h-4' })}
+      <div className="flex flex-row gap-2 items-center">
+        {cloneElement(props.icon, { className: "w-4 h-4" })}
         {props.children}
       </div>
     </LinkButton>
-  )
+  );
 }
 
 export function NavCollapsibleButton(props: {
-  links: { id: string; text: string }[]
-  icon: ReactElement
-  href: string
-  children?: any
+  links: { id: string; text: string }[];
+  icon: ReactElement;
+  href: string;
+  children?: any;
 }) {
-  const [opened, setOpened] = useState(true)
-  const pathname = usePathname()
+  const [opened, setOpened] = useState(true);
+  const pathname = usePathname();
 
   return (
-    <Collapsible open={opened} onOpenChange={setOpened} className='mb-1'>
+    <Collapsible open={opened} onOpenChange={setOpened} className="mb-1">
       <div>
         <CollapsibleTrigger asChild>
           <Button
-            variant={pathname.startsWith(props.href) ? 'default' : 'ghost'}
-            className='w-full justify-between mb-1'
+            variant={pathname.startsWith(props.href) ? "default" : "ghost"}
+            className="w-full justify-between mb-1"
           >
-            <div className='flex flex-row gap-2 items-center'>
-              {cloneElement(props.icon, { className: 'w-4 h-4' })}
+            <div className="flex flex-row gap-2 items-center">
+              {cloneElement(props.icon, { className: "w-4 h-4" })}
               {props.children}
             </div>
             <IconChevronDown
-              className={cn('w-4 h-4 transition-transform', {
-                'transform rotate-180': opened,
+              className={cn("w-4 h-4 transition-transform", {
+                "transform rotate-180": opened,
               })}
             />
           </Button>
         </CollapsibleTrigger>
         <CollapsibleContent>
-          <div className='flex flex-col gap-1 pl-5'>
-            {props.links.map(link => {
+          <div className="flex flex-col gap-1 pl-5">
+            {props.links.map((link) => {
               return (
                 <ContextMenu key={link.id}>
                   <ContextMenuTrigger>
                     <LinkButton
                       variant={
-                        pathname.startsWith('/app/courses/' + link.id)
-                          ? 'secondary'
-                          : 'ghost'
+                        pathname.startsWith("/app/courses/" + link.id)
+                          ? "secondary"
+                          : "ghost"
                       }
-                      size={'sm'}
-                      className='w-full justify-start'
-                      href={'/app/courses/' + link.id}
+                      size={"sm"}
+                      className="w-full justify-start"
+                      href={"/app/courses/" + link.id}
                     >
                       {link.text}
                     </LinkButton>
                   </ContextMenuTrigger>
-                  <ContextMenuContent className='w-64'>
+                  <ContextMenuContent className="w-64">
                     <ContextMenuItem>
                       <Link
-                        className='hover:no-underline cursor-default'
-                        href={'/app/courses/' + link.id}
+                        className="hover:no-underline cursor-default"
+                        href={"/app/courses/" + link.id}
                       >
                         Open
                       </Link>
@@ -101,11 +101,11 @@ export function NavCollapsibleButton(props: {
                     <ContextMenuItem>Change class image</ContextMenuItem>
                   </ContextMenuContent>
                 </ContextMenu>
-              )
+              );
             })}
           </div>
         </CollapsibleContent>
       </div>
     </Collapsible>
-  )
+  );
 }
--- src/app/sign-in/sign-in.tsx
+++ src/app/sign-in/sign-in.tsx
@@ -1,110 +1,177 @@
-'use client'
+"use client";
 
-import { Button } from '@/src/components/ui/button'
-import { Input } from '@/src/components/ui/input'
-import { Label } from '@/src/components/ui/label'
-import { useForm } from '@mantine/form'
-import { IconLoader } from '@tabler/icons-react'
-import { useSignIn } from '@clerk/nextjs'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { Button } from "@/src/components/ui/button";
+import { Input } from "@/src/components/ui/input";
+import { Label } from "@/src/components/ui/label";
+import { useForm } from "@mantine/form";
+import {
+  IconBrandApple,
+  IconBrandGithub,
+  IconBrandGoogle,
+  IconLoader,
+} from "@tabler/icons-react";
+import { useSignIn } from "@clerk/nextjs";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Separator } from "@/src/components/ui/separator";
+import { OAuthStrategy } from "@clerk/nextjs/dist/types/server";
+import Image from "next/image";
 
 export function SignIn({ redirectURL }: { redirectURL?: string }) {
-  const { isLoaded, signIn, setActive } = useSignIn()
+  const { isLoaded, signIn, setActive } = useSignIn();
 
   const form = useForm({
     initialValues: {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     },
 
     validate: {
-      email: value => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-      password: value => (value != '' ? null : 'Enter a password'),
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      password: (value) => (value != "" ? null : "Enter a password"),
     },
-  })
-  const router = useRouter()
-  const [loading, setLoading] = useState(false)
+  });
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const signInWith = (strategy: OAuthStrategy) => {
+    return signIn?.authenticateWithRedirect({
+      strategy,
+      redirectUrl: "/sso-callback",
+      redirectUrlComplete: "/app/home",
+    });
+  };
 
   return (
-    <>
+    <div className="overflow-scroll">
+      <div className="flex gap-5 items-center justify-between">
+        <Button
+          onClick={() => signInWith("oauth_apple")}
+          variant={"outline"}
+          size={"icon"}
+          className="h-12 grow"
+        >
+          <Image
+            alt="Apple Logo"
+            width={20}
+            height={20}
+            className="w-5 h-5 object-contain"
+            src="/logos/apple.svg"
+          />
+        </Button>
+        <Button
+          onClick={() => signInWith("oauth_github")}
+          variant={"outline"}
+          size={"icon"}
+          className="h-12 grow"
+        >
+          <Image
+            alt="Apple Logo"
+            width={20}
+            height={20}
+            className="w-5 h-5 object-contain"
+            src="/logos/github.svg"
+          />
+        </Button>
+        <Button
+          onClick={() => signInWith("oauth_google")}
+          variant={"outline"}
+          size={"icon"}
+          className="h-12 grow"
+        >
+          <Image
+            alt="Apple Logo"
+            width={20}
+            height={20}
+            className="w-5 h-5 object-contain"
+            src="/logos/google.svg"
+          />
+        </Button>
+      </div>
+      <div className="flex justify-center items-center flex-nowrap my-6 w-full">
+        <Separator className="shrink" />
+        <p className="mx-3 h-2 flex items-center justify-center text-muted-foreground">
+          or
+        </p>
+        <Separator className="shrink" />
+      </div>
       <form
-        onSubmit={form.onSubmit(async values => {
+        onSubmit={form.onSubmit(async (values) => {
           try {
-            setLoading(true)
+            setLoading(true);
             await signIn
               ?.create({
                 identifier: values.email,
                 password: values.password,
               })
-              .then(res => {
-                console.log(res)
-                res?.status != 'complete'
+              .then((res) => {
+                console.log(res);
+                res?.status != "complete"
                   ? form.setErrors({
-                      email: 'Email or password is incorrect',
-                      password: 'Email or password is incorrect',
+                      email: "Email or password is incorrect",
+                      password: "Email or password is incorrect",
                     })
-                  : setActive({ session: res.createdSessionId })
-                setLoading(false)
-              })
+                  : setActive({ session: res.createdSessionId });
+                setLoading(false);
+              });
           } catch (err: any) {
-            console.error('error', err.errors[0].longMessage)
-            setLoading(false)
+            console.error("error", err.errors[0].longMessage);
+            setLoading(false);
           }
         })}
       >
-        <div className='flex flex-col gap-4 max-w-[400px]'>
-          <div className='grid w-full items-center gap-1.5'>
-            <Label htmlFor='email'>Email Address</Label>
+        <div className="flex flex-col gap-4 max-w-[400px]">
+          <div className="grid w-full items-center gap-1.5">
+            <Label htmlFor="email">Email Address</Label>
             <Input
-              className='text-primary'
-              placeholder='example@example.com'
+              className="text-primary"
+              placeholder="example@example.com"
               disabled={!isLoaded || loading}
-              id='email'
-              type='email'
+              id="email"
+              type="email"
               // @ts-ignore
               style={
                 form.errors.email
-                  ? { '--input': '0 100% 50%', color: 'hsl(0 100% 50%)' }
+                  ? { "--input": "0 100% 50%", color: "hsl(0 100% 50%)" }
                   : null
               }
-              {...form.getInputProps('email')}
+              {...form.getInputProps("email")}
             />
           </div>
-          <div className='flex flex-col gap-0'>
-            <div className='grid w-full items-center gap-1.5'>
-              <Label htmlFor='password'>Password</Label>
+          <div className="flex flex-col gap-0">
+            <div className="grid w-full items-center gap-1.5">
+              <Label htmlFor="password">Password</Label>
               <Input
-                className='text-primary'
-                placeholder='securepassword123'
-                type='password'
+                className="text-primary"
+                placeholder="securepassword123"
+                type="password"
                 disabled={!isLoaded || loading}
-                id='password'
+                id="password"
                 // @ts-ignore
                 style={
                   form.errors.password
-                    ? { '--input': '0 100% 50%', color: 'hsl(0 100% 50%)' }
+                    ? { "--input": "0 100% 50%", color: "hsl(0 100% 50%)" }
                     : null
                 }
-                {...form.getInputProps('password')}
+                {...form.getInputProps("password")}
               />
-              <p className='text-xs text-muted-foreground'>
-                Forgot your password?{' '}
-                <Link target='blank' href={'./forgot'}>
+              <p className="text-xs text-muted-foreground">
+                Forgot your password?{" "}
+                <Link target="blank" href={"./forgot"}>
                   Click here
                 </Link>
               </p>
             </div>
           </div>
-          <Button type='submit' disabled={!isLoaded || loading}>
+          <Button type="submit" disabled={!isLoaded || loading}>
             {!isLoaded || loading ? (
-              <IconLoader className='h-4 w-4 animate-spin' />
+              <IconLoader className="h-4 w-4 animate-spin" />
             ) : null}
             Submit
           </Button>
         </div>
       </form>
-    </>
-  )
+    </div>
+  );
 }
