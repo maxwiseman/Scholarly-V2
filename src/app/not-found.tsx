2ee722362f38f82461061b2cd20c4132ba3c975c
maxwiseman <13maxwellwise@gmail.com>
2023-07-25 21:50:11 -0400

    More changes to the 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,26 +8,29 @@ export const metadata = {
 export default function NotFound() {
   return (
     <div className="w-screen h-screen flex justify-center items-center flex-col gap-5">
-      <div className="relative">
-        {/* <h1 className="absolute text-9xl font-black text-center w-full bottom-full">
-          404
-        </h1> */}
-        <div className="h-72 w-[34rem] relative">
+      <h1
+        className="text-9xl font-black text-center w-max bottom-full mb-[-1rem]"
+        style={{
+          textShadow: "1px 5px 14px rgba(0,0,0,0.36)",
+        }}
+      >
+        404
+      </h1>
+      {/* <div className="h-72 w-[34rem] relative">
           <Image
             className="object-cover"
             src={"/404.gif"}
             alt="404: Page not found"
             fill
           />
-        </div>
-        <p
-          style={{ boxShadow: "0px -47px 77px -41px rgba(255,255,255,1);" }}
-          className="text-center"
-        >
-          The resource you requested does not exist
-        </p>
+        </div> */}
+      <p className="text-center">{`This page doesn't exist`}</p>
+      <div className="flex gap-2">
+        <LinkButton href="/">Main site</LinkButton>
+        <LinkButton href="/app/home" variant={"secondary"}>
+          Home screen
+        </LinkButton>
       </div>
-      <LinkButton href="/">Back home</LinkButton>
     </div>
   );
 }
