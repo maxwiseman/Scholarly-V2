6b18d0a6b179ec28d414d5d006f0eef65885edae
maxwiseman <13maxwellwise@gmail.com>
2023-08-14 21:46:07 -0400

    Updated schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "drizzle-kit";
+import { URL } from "./dbconfig";
 
 export default {
   schema: "./src/database/schema.ts",
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    connectionString: process.env.DIRECT_URL + "?sslmode=require",
+    connectionString: URL,
   },
 } satisfies Config;
