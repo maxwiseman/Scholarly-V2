9af208ac96835d40169e1d85dde126b327f6e91e
maxwiseman <13maxwellwise@gmail.com>
2023-08-23 07:47:45 -0400

    Fixed error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from "drizzle-kit";
-import { URL } from "./dbconfig";
+// import { URL } from "./dbconfig";
 
 export default {
   schema: "./src/database/schema.ts",
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    connectionString: URL,
+    connectionString: "",
   },
 } satisfies Config;
